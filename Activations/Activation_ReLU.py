5e5084bf0aa5fca9b3af34664d0d6fecf0592734
VenkateshRoshan <butchivenkatesh.a@gmail.com>
2020-12-31 10:42:42 +0530

    ReLU_1.0 (ReLU class and its methods defined)

---
+++ Activations/Activation_ReLU.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class ReLU : # ReLU - Rectified Linear Unit
+
+	def __init__(self) :
+		self.__Name__ = 'ReLU' # __Name__ initialization
+		self.__type__ = 'activation'
+
+	def feed(self,X) :
+
+		"""
+			The function returns 0 if it receives any negative input, but for any positive value  x  it returns that value back. 
+			So it can be written as  f(x)=max(0,x) .
+		"""
+
+		self.output = np.maximum(0,X) 
+		return self.output # returning output after applying activation
