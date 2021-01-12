72daf79e642139b50e4df7d6f30fd9a3eb1fc8af
VenkateshRoshan <butchivenkatesh.a@gmail.com>
2021-01-12 17:36:11 +0530

    Dense_1.2 (dense method fixed)

--- Layers/Layer_Dense.py
+++ Layers/Layer_Dense.py
@@ -49,21 +49,32 @@ class Dense() :
 
 	def dense(self,input_batch) :
 		self.output_batch = []
-		self.input.append(input_batch)
-		weights = 0.10 * np.random.randn(len(input_batch[0]),self.NUM_FILTERS) / np.sqrt(len(input_batch[0])+self.NUM_FILTERS)
-		self.WEIGHTS.append(weights)
+		self.input = input_batch
+		self.WEIGHTS = 0.01*np.random.randn(len(input_batch[0]),self.NUM_FILTERS) / np.sqrt(len(input_batch[0])+self.NUM_FILTERS)
+
+		self.bias = np.random.randn(len(input_batch), self.NUM_FILTERS) / np.sqrt(len(input_batch[0])+self.NUM_FILTERS)
 		
-		self.output_batch = np.dot(np.array(input_batch),weights)
+		self.output_batch = np.dot(np.array(input_batch),self.WEIGHTS) + self.bias
 		return self.output_batch
 
 	def feed(self,X_train) :
 		return self.dense(X_train)
 
 	def feed_backward(self,out_err,lr) :
+		"""
+			Error measuring and updating weights and bias to improve accuracy
+		"""
 		self.input = np.array(self.input)
-		input_error = np.dot(out_err,self.WEIGHTS.T)
-		self.WEIGHTS -= lr*np.dot(self.input.T,out_err)
-		return input_error
+		self.WEIGHTS = np.array(self.WEIGHTS)
+		input_error = []
+		for i in out_err :
+			input_error.append(np.dot(i,self.WEIGHTS.T))
+		weights_err = []
+		for i in np.dot(self.input.T,out_err) :
+			weights_err.append(i)
+		self.WEIGHTS -= lr*np.array(weights_err)
+		self.bias -= lr * out_err
+		return np.array(input_error)
 
 	def Summary(self) :
 		print(f'{self.__Name__}\t\t{self.input_shape}\t\t{self.output_shape}')
@@ -78,15 +89,14 @@ def main() :
 		img = cv2.resize(img,shape[:2])
 		img = img/255.
 		X_train.append(img)
-	WEIGHTS = np.ones((len(X_train)))
 	conv = Conv2D(NUM_FILTERS=16,KERNEL_SIZE=3,input_shape=inp.output_shape)
 	X_train = np.array(X_train)
 	maxpool = MaxPool2D(KERNEL_SIZE=3,STRIDES=3,input_shape=conv.output_shape)
-	output = maxpool.feed(conv.feed(X_train,WEIGHTS))
+	output = maxpool.feed(conv.feed(X_train))
 	flat = Flatten(input_shape=maxpool.output_shape)
 	output = flat.feed(output)
 	dense = Dense(32,input_shape=flat.output_shape)
-	output = dense.feed(output,WEIGHTS)
+	output = dense.feed(output)
 	print(output)
 
 if __name__ == '__main__':
